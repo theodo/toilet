3c59db4d2c58e39295c86d179552c338c79fb331
Benjamin Grandfond <benjaming@theodo.fr>
2012-02-03 01:51:20 +0100

    Add comments on fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,13 @@
-__author__ = 'benjamin'
+# -*- coding: utf-8 -*-
+__author__ = 'Benjamin Grandfond <benjaming@theodo.fr>'
 
 from fabric.api import *
 from fabric.tasks import Task
 
 class TestTask(Task):
+    """
+    Run toilet tests.
+    """
     name = 'test'
 
     def run(self):
